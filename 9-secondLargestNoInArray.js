f20b05b0eefe9d09869bb011ef5815e3eba9f5f6
Akshaya Singh <akshaysing975@gmail.com>
2025-06-22 22:00:07 +0530

    check corner cases of second largest no in array and modified according to that

--- 9-secondLargestNoInArray.js
+++ 9-secondLargestNoInArray.js
@@ -1,16 +1,22 @@
 function secondLargestNumber(arr) {
-    let largest = -Infinity;
-    let secondLargest = -Infinity;
+  if (arr.length < 2) {
+    return "Array should have atleast 2 numbers";
+  }
 
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i] > largest){
-            secondLargest = largest;
-            largest = arr[i];
-        }
+  let largest = -Infinity;
+  let secondLargest = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > largest) {
+      secondLargest = largest;
+      largest = arr[i];
+    } else if (arr[i] > secondLargest && arr[i] !== largest) {
+      secondLargest = arr[i];
     }
-    return secondLargest;
+  }
+  return secondLargest;
 }
 
-let arr = [2, -6, 4, 8,10, 1, -9];
+let arr = [2, -6, 4, 8, 10, 1, -9];
 const result = secondLargestNumber(arr);
 console.log(result);
