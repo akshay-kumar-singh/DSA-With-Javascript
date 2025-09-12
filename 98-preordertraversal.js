780357ae9270a9897929ece687d8817b1fcde543
Akshaya Singh <akshaysing975@gmail.com>
2025-09-12 08:40:15 +0530

    Preorder Traversal - Iterative Approach

--- 98-preordertraversal.js
+++ 98-preordertraversal.js
@@ -10,3 +10,21 @@ var preorderTraversal = function (root) {
   traversal(root);
   return ans;
 };
+
+
+//Iterative Approach
+var preorderTraversal = function(root) {
+    if (!root) return [];
+    let ans = [];
+    let stack = [root];
+
+    while (stack.length) {
+        let curr = stack.pop();
+        ans.push(curr.val);
+
+        if (curr.right) stack.push(curr.right);
+        if (curr.left) stack.push(curr.left);
+    }
+
+    return ans;
+};
