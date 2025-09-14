28a9eb132d654922036a346913562736508bd3c5
Akshaya Singh <akshaysing975@gmail.com>
2025-09-14 08:53:49 +0530

    Postorder - Iterative Approach - 2 Stacks

--- 99-post&inordertraversal.js
+++ 99-post&inordertraversal.js
@@ -47,3 +47,25 @@ var postorderTraversal = function (root) {
   traversal(root);
   return ans;
 };
+
+
+//Iterative Approach using 2 stack
+
+var postorderTraversal = function(root){
+    if(!root) return [];
+    let s1 = [root];
+    let s2 = [];
+
+    while(s1.length){
+        let curr = s1.pop();
+        s2.push(curr);
+        curr.left && s1.push(curr.left);
+        curr.right && s1.push(curr.right);
+    }
+
+    let ans = [];
+    while(s2.length){
+        ans.push(s2.pop().val);
+    }
+    return ans;
+}
