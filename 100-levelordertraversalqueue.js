325f3a9a4643ee8502e95c93108b4a33a90fabfd
Akshaya Singh <akshaysing975@gmail.com>
2025-09-17 07:46:22 +0530

    Level Order Traversal - Recursive Approach

--- 100-levelordertraversalqueue.js
+++ 100-levelordertraversalqueue.js
@@ -1,20 +1,35 @@
-var levelOrder = function(root) {
-    if(!root) return [];
-    let q = [root];
-    let ans = [];
+var levelOrder = function (root) {
+  if (!root) return [];
+  let q = [root];
+  let ans = [];
 
-    while(q.length){
-        let levelArr = [];
-        let levelSize = q.length;
+  while (q.length) {
+    let levelArr = [];
+    let levelSize = q.length;
 
-        for(let i = 0; i < levelSize; i++){
-            let curr = q.shift();
-            curr.left && q.push(curr.left);
-            curr.right && q.push(curr.right);
-            levelArr.push(curr.val)
-        }
-        ans.push(levelArr);
+    for (let i = 0; i < levelSize; i++) {
+      let curr = q.shift();
+      curr.left && q.push(curr.left);
+      curr.right && q.push(curr.right);
+      levelArr.push(curr.val);
     }
-    return ans;
+    ans.push(levelArr);
+  }
+  return ans;
+};
 
+//Recursive Approach
+
+var levelOrder = function (root) {
+  if (!root) return [];
+  let ans = [];
+
+  let traversal = (curr, level) => {
+    if (!ans[level]) ans[level] = [];
+    ans[level].push(curr.val);
+    curr.left && traversal(curr.left, level + 1);
+    curr.right && traversal(curr.right, level + 1);
+  };
+  traversal(root, 0);
+  return ans;
 };
