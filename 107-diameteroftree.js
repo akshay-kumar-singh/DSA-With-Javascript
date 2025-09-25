d128b686d722d2dc58696544df434f8d258fbf32
Akshaya Singh <akshaysing975@gmail.com>
2025-09-25 10:31:13 +0530

    Solved : Diameter of a Binary Tree

---
+++ 107-diameteroftree.js
@@ -0,0 +1,13 @@
+var diameterOfBinaryTree = function(root) {
+  let maxDiameter = 0;
+  let findDepth = (curr) => {
+    if(!curr) return 0;
+    let leftDepth = findDepth(curr.left);
+    let rightDepth = findDepth(curr.right);
+    let currDiameter = leftDepth + rightDepth;
+    maxDiameter = Math.max(currDiameter, maxDiameter);
+    return 1 + Math.max(leftDepth, rightDepth);
+  }  
+  findDepth(root);
+  return maxDiameter;
+};
