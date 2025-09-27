ac935ca4d3e9949236551c0ad158e468973f22ba
Akshaya Singh <akshaysing975@gmail.com>
2025-09-27 09:33:06 +0530

    Solved : Subtree of another Tree

---
+++ 109-subtreeofanothertree.js
@@ -0,0 +1,20 @@
+var isSubtree = function(root, subRoot) {
+    let hashRoot = serialize(root);
+    let hashSubRoot = serialize(subRoot);
+
+    return hashRoot.includes(hashSubRoot);
+};
+let serialize = function(root) {
+    let hash = "";
+    let traversal = (curr) => {
+        if(!curr) {
+            hash = hash + "-#";
+            return;
+        }
+        hash = hash + "-" + curr.val;
+        traversal(curr.left);
+        traversal(curr.right);
+    }
+    traversal(root);
+    return hash;
+}    
