8fed245fe1dd373470259d1f9645610c332012e5
Akshaya Singh <akshaysing975@gmail.com>
2025-10-08 09:12:08 +0530

    Kth Largest Element in an Array

---
+++ 120-kthlargesteleinarray.js
@@ -0,0 +1,10 @@
+var findKthLargest = function(nums, k) {
+    let pq = new MinPriorityQueue();
+    for(let i = 0; i < nums.length; i++){
+        pq.enqueue(nums[i]);
+        if(pq.size() > k){
+            pq.dequeue();
+        }
+    }
+    return pq.front();
+};
