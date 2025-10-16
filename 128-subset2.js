22a520a1245b83060046a2ad534e386631e0bfc1
Akshaya Singh <akshaysing975@gmail.com>
2025-10-16 08:52:46 +0530

    Solved : Subset 2

---
+++ 128-subset2.js
@@ -0,0 +1,19 @@
+var subsetsWithDup = function(arr) {
+    let result = [];
+    arr.sort((a, b) => a - b);
+
+    let backtrack = (path, start) => {
+        result.push([...path]);
+        for (let i = start; i < arr.length; i++) {
+            // skip duplicates
+            if (i > start && arr[i - 1] === arr[i]) continue;
+
+            path.push(arr[i]);
+            backtrack(path, i + 1);
+            path.pop();
+        }
+    }
+
+    backtrack([], 0);
+    return result;
+};
