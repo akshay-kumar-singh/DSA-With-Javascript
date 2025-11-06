dbff1aafc2d0bb5e7a628dbdb79bda560087f263
Akshaya Singh <akshaysing975@gmail.com>
2025-11-06 07:48:31 +0530

    Solved: Candy (one pass)

--- 150-candy.js
+++ 150-candy.js
@@ -1,3 +1,4 @@
+//Candy Two Pass
 var candy = function(arr) {
     let n = arr.length;
     let ltr = Array(n).fill(1);
@@ -23,5 +24,34 @@ var candy = function(arr) {
         ans += Math.max(rtl[i], ltr[i]);
     }
 
+    return ans;
+};
+
+
+//Candy One Pass
+var candy = function(arr) {
+    let n = arr.length;
+    let ans = n;
+    let i = 1;
+
+    while(i < n){
+        if(arr[i] === arr[i-1]) {
+            ++i;
+            continue;
+        }
+        let up = 0;
+        while(i < n && arr[i] > arr[i-1]){
+            ++up;
+            ans = ans + up;
+            ++i;
+        }
+        let down = 0;
+        while(i < n && arr[i] < arr[i-1]) {
+            ++down;
+            ans += down;
+            ++i;
+        }
+        ans = ans - Math.min(up, down);
+    }
     return ans;
 };
