4bdfdf2533353e1f0c7ade771c7e814df8093c87
Akshaya Singh <akshaysing975@gmail.com>
2025-11-27 07:49:09 +0530

    Find if path exist dfs recursive

--- 168-findifpathexist.js
+++ 168-findifpathexist.js
@@ -25,3 +25,31 @@ var validPath = function(n, edges, source, destination) {
     return false;
 
 };
+
+//DFS Recursive 
+var validPath = function (n, edges, source, destination) {
+    let map = {};
+    for (let [x, y] of edges) {
+        if (!map[x]) map[x] = [];
+        if (!map[y]) map[y] = [];
+        map[x].push(y);
+        map[y].push(x);
+    }
+    let q = [source];
+    let visited = new Set();
+    let dfs = (curr) => {
+        if (curr === destination) {
+            return true;
+        }
+        visited.add(curr);
+        for (let neighbor of map[curr]) {
+            if (!visited.has(neighbor)) {
+                if (dfs(neighbor)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    return dfs(source);
+};
